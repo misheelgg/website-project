3338b17c469e41d3f14e745edc7bd3639f0c5176
misheelgg <misheel.g@gmail.com>
2025-08-15 10:43:11 +0800

    Delete degree-programs.js

--- degree-programs.js
+++
@@ -1,6 +0,0 @@
-const degTypeButton = document.querySelectorAll('.deg-type-button');
-
-degTypeButton.addEventListener('click', function() {
-  degTypeButton.style.border = '3px solid black';
-  degTypeButton.style.color = 'rgb(111, 111, 112)';
-});
